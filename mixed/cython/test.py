1e4b462ee1ea995a580997fc01356741a90b562a
Simon Funke <simon.funke@gmail.com>
2016-09-27 21:48:13 +0200

    Adjust parameters a bit

--- mixed/cython/test.py
+++ mixed/cython/test.py
@@ -3,15 +3,15 @@ import integral_py
 import integral_semipy
 import integral
 
-print("Pure Python integrate: {}".format(integral_py.integrate_f(1.0, 10.0, 10000)))
-print("Semi (C)Python integrate: {}".format(integral_semipy.integrate_f(1.0, 10.0, 10000)))
-print("Cython integrate: {}".format(integral.integrate_f(1.0, 10.0, 10000)))
+print("Pure Python integrate: {}".format(integral_py.integrate_f(1.0, 10.0, 1000000)))
+print("Semi (C)Python integrate: {}".format(integral_semipy.integrate_f(1.0, 10.0, 1000000)))
+print("Cython integrate: {}".format(integral.integrate_f(1.0, 10.0, 1000000)))
 
-N = 1000
+N = 10
 print("")
 print("Running timings ({} integrations)".format(N))
 
-print("Pure Python integrate: {}".format(timeit("integral_py.integrate_f(1.0, 10.0, 10000)", setup="import integral_py", number=N)))
-print("Semi (C)Python integrate: {}".format(timeit("integral_semipy.integrate_f(1.0, 10.0, 10000)", setup="import integral_semipy", number=N)))
-print("Cython integrate: {}".format(timeit("integral.integrate_f(1.0, 10.0, 10000)", setup="import integral", number=N)))
+print("Pure Python integrate: {}".format(timeit("integral_py.integrate_f(1.0, 10.0, 1000000)", setup="import integral_py", number=N)))
+print("Semi (C)Python integrate: {}".format(timeit("integral_semipy.integrate_f(1.0, 10.0, 1000000)", setup="import integral_semipy", number=N)))
+print("Cython integrate: {}".format(timeit("integral.integrate_f(1.0, 10.0, 1000000)", setup="import integral", number=N)))
 
