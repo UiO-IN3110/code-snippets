905ccbcef95414fd2f1db38e68f45f35639c77f9
Simon Funke <simon.funke@gmail.com>
2016-09-27 21:38:54 +0200

    Add timeit timings

--- mixed/cython/test.py
+++ mixed/cython/test.py
@@ -1,9 +1,17 @@
+from timeit import timeit
 import integral_py
 import integral_semipy
 import integral
 
-print("Integrate: {}".format(integral_py.integrate_f(1.0, 10.0, 10000)))
-print("Integrate: {}".format(integral_semipy.integrate_f(1.0, 10.0, 10000)))
-print("Integrate: {}".format(integral.integrate_f(1.0, 10.0, 10000)))
+print("Pure Python integrate: {}".format(integral_py.integrate_f(1.0, 10.0, 10000)))
+print("Semi (C)Python integrate: {}".format(integral_semipy.integrate_f(1.0, 10.0, 10000)))
+print("Cython integrate: {}".format(integral.integrate_f(1.0, 10.0, 10000)))
 
+N = 1000
+print("")
+print("Running timings ({} integrations)".format(N))
+
+print("Pure Python integrate: {}".format(timeit("integral_py.integrate_f(1.0, 10.0, 10000)", setup="import integral_py", number=N)))
+print("Semi (C)Python integrate: {}".format(timeit("integral_semipy.integrate_f(1.0, 10.0, 10000)", setup="import integral_semipy", number=N)))
+print("Cython integrate: {}".format(timeit("integral.integrate_f(1.0, 10.0, 10000)", setup="import integral", number=N)))
 
