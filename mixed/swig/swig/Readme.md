2fa7982d456365e9e04f247adcd79ef3bfbc1db1
Simon Funke <simon.funke@gmail.com>
2016-10-03 12:00:33 +0200

    Move location of c files

--- mixed/swig/swig/Readme.md
+++ mixed/swig/swig/Readme.md
@@ -1,12 +1,12 @@
 #Generate swig interface with:
 
-swig -python -I.. hw.i
+swig -python -Isrc hw.i
 
 #Look at generated wrapper in hw_wrap.c
 
 
 #Compile with:
 
-gcc -I.. -fPIC $(pkg-config --cflags --libs python3) -c ../hw.c hw_wrap.c
+gcc -Isrc -fPIC $(pkg-config --cflags --libs python3) -c src/hw.c hw_wrap.c
 
 gcc -shared -fPIC -o _hw.so hw.o hw_wrap.o
--- mixed/swig/swig/setup.py
+++ mixed/swig/swig/setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup, Extension
 name = "hw"    # name of the module
 version = 1.0  # the module's version number
 
-swig_cmd = "swig -python -I.. %s.i" % name
+swig_cmd = "swig -python -Isrc %s.i" % name
 
 print "running SWIG:", swig_cmd
 os.system(swig_cmd)
 
-sources = ["../hw.c", "hw_wrap.c"]
+sources = ["src/hw.c", "hw_wrap.c"]
 setup(name=name, version=version,
       ext_modules=[Extension('_' + name, # SWIG requires _
                    sources, include_dirs=[os.pardir])
