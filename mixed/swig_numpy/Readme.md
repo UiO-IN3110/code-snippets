1cacc38e628fd29c4224889093e18de33f32ad48
Simon Funke <simon.funke@gmail.com>
2016-10-05 09:57:22 +0200

    Better formatting

--- mixed/swig_numpy/Readme.md
+++ mixed/swig_numpy/Readme.md
@@ -1,9 +1,9 @@
-A Swig numpy demo, taken from https://gist.github.com/mattbierbaum/1189856
+A Swig numpy demo, motivated from https://gist.github.com/mattbierbaum/1189856
 
 Install with
 
 ```bash
 python setup.py build_ext
-python setup.py install --user
+python setup.py install --install-platlib=.
 python test.py
 ```
--- mixed/swig_numpy/setup.py
+++ mixed/swig_numpy/setup.py
@@ -2,6 +2,9 @@ from distutils.core import setup, Extension
 import numpy
 import os
 
-os.environ['CC'] = 'g++';
-setup(name='matt_simple_test', version='1.0', ext_modules =[Extension('_simple',
- ['simple.cc', 'simple.i'], include_dirs = [numpy.get_include(),'.'])])
+setup(name='simple',
+      version='1.0',
+      ext_modules =[Extension('_simple',
+                             ['simple.cc', 'simple.i'],
+                    include_dirs = [numpy.get_include(),'.'])
+                   ])
