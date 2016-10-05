18cd3def01222d9c25da4297251c189c9b58cf3f
Simon Funke <simon.funke@gmail.com>
2016-10-05 09:58:17 +0200

    Rename simple.cc -> simple.c

--- mixed/swig_numpy/setup.py
+++ mixed/swig_numpy/setup.py
@@ -5,6 +5,6 @@ import os
 setup(name='simple',
       version='1.0',
       ext_modules =[Extension('_simple',
-                             ['simple.cc', 'simple.i'],
+                             ['simple.c', 'simple.i'],
                     include_dirs = [numpy.get_include(),'.'])
                    ])
