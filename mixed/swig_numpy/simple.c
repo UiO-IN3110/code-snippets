769bd3f346c60422a57de64171dd0a665dc5bffb
Simon Funke <simon.funke@gmail.com>
2016-10-12 16:44:17 +0200

    Update simple.c to be C89 compliant

--- mixed/swig_numpy/simple.c
+++ mixed/swig_numpy/simple.c
@@ -1,12 +1,16 @@
 double dot(int n, double *a, int m, double *b){
+  int i;
+
   double sum = 0.0;
-  for (int i=0; i<n; i++){
+  for (i=0; i<n; i++){
     sum += a[i]*b[i];
   }
   return sum;
 }
 
 void arange(int size, double *arr){
-  for (int i=0; i<size; i++)
+  int i;
+
+  for (i=0; i<size; i++)
     arr[i] = i;
 }
